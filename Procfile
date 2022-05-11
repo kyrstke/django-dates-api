62f376af37a633e94eb16f519df7e8bc2c23b31e
Bambo <kyrstke@gmail.com>
2022-05-11 17:31:43 +0200

    Debugged settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn RESTDatesAPI.wsgi
--- RESTDatesAPI/settings.py
+++ RESTDatesAPI/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
-# STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",

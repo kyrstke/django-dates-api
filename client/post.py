617c1cc133e9bfdaa580a75e97e7cc80be2cc276
Bambo <kyrstke@gmail.com>
2022-05-13 15:56:10 +0200

    Modified client/post.py

--- client/post.py
+++ client/post.py
@@ -3,8 +3,8 @@ import requests
 endpoint = "http://localhost:8000/dates/"
 
 data = {
-    "month": 11,
-    "day": 11
+    "month": 9,
+    "day": 19
 }
 get_response = requests.post(endpoint, json=data)
 get_response.raise_for_status()
